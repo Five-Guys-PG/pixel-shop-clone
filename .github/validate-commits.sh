45b80c9f5d6624e232d65b836907b9ed81653fc0
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-10-06 12:27:27 +0200

    fix: validation script

--- .github/validate-commits.sh
+++ .github/validate-commits.sh
@@ -3,7 +3,7 @@
 CURRENT_BRANCH=${GITHUB_HEAD_REF}
 echo "Checking commit messages for ${CURRENT_BRANCH} against dev"
 
-git fetch origin
+git fetch origin dev:dev
 BASE_BRANCH=$(git merge-base dev ${CURRENT_BRANCH})
 
 if git log --format="%s" $BASE_BRANCH..${CURRENT_BRANCH} | grep -vE '^(init|feat|fix|docs|chore|refactor)( #[0-9]+)?: .{1,100}'
