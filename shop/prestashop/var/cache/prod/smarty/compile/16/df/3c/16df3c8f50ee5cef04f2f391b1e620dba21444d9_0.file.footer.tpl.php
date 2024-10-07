c8d3af0541bcde41b0e142f168c171cd6c4e5139
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-10-07 21:08:20 +0200

    fix: update .gitignore

---
+++ shop/prestashop/var/cache/prod/smarty/compile/16/df/3c/16df3c8f50ee5cef04f2f391b1e620dba21444d9_0.file.footer.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-10-07 18:43:17
+  from '/var/www/html/admin2506zyhrp/themes/new-theme/template/footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_67040fa5286a54_41413384',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '16df3c8f50ee5cef04f2f391b1e620dba21444d9' => 
+    array (
+      0 => '/var/www/html/admin2506zyhrp/themes/new-theme/template/footer.tpl',
+      1 => 1702485415,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67040fa5286a54_41413384 (Smarty_Internal_Template $_smarty_tpl) {
+?><div id="footer" class="bootstrap">
+    <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['hook'][0], array( array('h'=>"displayBackOfficeFooter"),$_smarty_tpl ) );?>
+
+</div>
+<?php }
+}
---
+++ shop/prestashop/var/cache/prod/smarty/compile/classiclayouts_layout_left_column_tpl/35/49/8c/35498cde90bd44dbd1cf3c0ad2862b8545e30b03_2.file.notifications.tpl.php
@@ -0,0 +1,186 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-10-06 20:35:51
+  from '/var/www/html/themes/classic/templates/_partials/notifications.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_6702d8876cacb5_49054167',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '35498cde90bd44dbd1cf3c0ad2862b8545e30b03' => 
+    array (
+      0 => '/var/www/html/themes/classic/templates/_partials/notifications.tpl',
+      1 => 1702485415,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6702d8876cacb5_49054167 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+
+<?php if ((isset($_smarty_tpl->tpl_vars['notifications']->value))) {?>
+<aside id="notifications">
+  <div class="container">
+    <?php if ($_smarty_tpl->tpl_vars['notifications']->value['error']) {?>
+      <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_16989627026702d8876c7217_03650043', 'notifications_error');
+?>
+
+    <?php }?>
+
+    <?php if ($_smarty_tpl->tpl_vars['notifications']->value['warning']) {?>
+      <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1692682736702d8876c83d3_26750808', 'notifications_warning');
+?>
+
+    <?php }?>
+
+    <?php if ($_smarty_tpl->tpl_vars['notifications']->value['success']) {?>
+      <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_17824351996702d8876c92e9_39681475', 'notifications_success');
+?>
+
+    <?php }?>
+
+    <?php if ($_smarty_tpl->tpl_vars['notifications']->value['info']) {?>
+      <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1261935616702d8876ca0d5_22129020', 'notifications_info');
+?>
+
+    <?php }?>
+  </div>
+</aside>
+<?php }
+}
+/* {block 'notifications_error'} */
+class Block_16989627026702d8876c7217_03650043 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'notifications_error' => 
+  array (
+    0 => 'Block_16989627026702d8876c7217_03650043',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <article class="alert alert-danger" role="alert" data-alert="danger">
+          <ul>
+            <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['notifications']->value['error'], 'notif');
+$_smarty_tpl->tpl_vars['notif']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['notif']->value) {
+$_smarty_tpl->tpl_vars['notif']->do_else = false;
+?>
+              <li><?php echo $_smarty_tpl->tpl_vars['notif']->value;?>
+</li>
+            <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+          </ul>
+        </article>
+      <?php
+}
+}
+/* {/block 'notifications_error'} */
+/* {block 'notifications_warning'} */
+class Block_1692682736702d8876c83d3_26750808 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'notifications_warning' => 
+  array (
+    0 => 'Block_1692682736702d8876c83d3_26750808',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <article class="alert alert-warning" role="alert" data-alert="warning">
+          <ul>
+            <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['notifications']->value['warning'], 'notif');
+$_smarty_tpl->tpl_vars['notif']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['notif']->value) {
+$_smarty_tpl->tpl_vars['notif']->do_else = false;
+?>
+              <li><?php echo $_smarty_tpl->tpl_vars['notif']->value;?>
+</li>
+            <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+          </ul>
+        </article>
+      <?php
+}
+}
+/* {/block 'notifications_warning'} */
+/* {block 'notifications_success'} */
+class Block_17824351996702d8876c92e9_39681475 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'notifications_success' => 
+  array (
+    0 => 'Block_17824351996702d8876c92e9_39681475',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <article class="alert alert-success" role="alert" data-alert="success">
+          <ul>
+            <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['notifications']->value['success'], 'notif');
+$_smarty_tpl->tpl_vars['notif']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['notif']->value) {
+$_smarty_tpl->tpl_vars['notif']->do_else = false;
+?>
+              <li><?php echo $_smarty_tpl->tpl_vars['notif']->value;?>
+</li>
+            <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+          </ul>
+        </article>
+      <?php
+}
+}
+/* {/block 'notifications_success'} */
+/* {block 'notifications_info'} */
+class Block_1261935616702d8876ca0d5_22129020 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'notifications_info' => 
+  array (
+    0 => 'Block_1261935616702d8876ca0d5_22129020',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <article class="alert alert-info" role="alert" data-alert="info">
+          <ul>
+            <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['notifications']->value['info'], 'notif');
+$_smarty_tpl->tpl_vars['notif']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['notif']->value) {
+$_smarty_tpl->tpl_vars['notif']->do_else = false;
+?>
+              <li><?php echo $_smarty_tpl->tpl_vars['notif']->value;?>
+</li>
+            <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+          </ul>
+        </article>
+      <?php
+}
+}
+/* {/block 'notifications_info'} */
+}
