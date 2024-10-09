07729afa490a6984d8eaea6e3df79a18da06aed7
Artem Dychenko <artemdychenko@gmail.com>
2024-10-09 20:49:35 +0300

    fix: improve delivery info detection; feat: change data structure written to JSON

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,123 +1,192 @@
-import requests
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
+import requests
 import json
 import os
 
-site_url = 'https://pixel-shop.pl'
+
+firefox_options = Options()
+firefox_options.add_argument("--headless")
+firefox_service = Service("/snap/bin/geckodriver")
+
+# Initialization firefox browser
+driver = webdriver.Firefox(service=firefox_service, options=firefox_options)
+
+site_url = "https://pixel-shop.pl"
 
 
+def setup_page_for_scraping_using_selenium(site_url):
+    # Opening of site
+    driver.get(site_url)
+
+    # Removing the preloader
+    driver.execute_script("""
+        var preloader = document.getElementById('pixel-preloader');
+        if (preloader) {
+            preloader.style.display = 'none';
+        }
+    """)
+
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.invisibility_of_element_located((By.ID, "pixel-preloader"))
+        )
+    except TimeoutException:
+        print("Preloader nie zniknął w wyznaczonym czasie.")
+
+    time.sleep(2)
+
+    # Closing the cookies module
+    try:
+        accept_cookies_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "js__accept-all-consents"))
+        )
+        accept_cookies_button.click()
+    except Exception as e:
+        print("Modal cookies nie znaleziony lub już zamknięty:", str(e))
 
 
-def scrape_delivery_info(delivery_div):
+def scrape_delivery_info(full_url):
     delivery_data = []
 
-    # Extract the delivery methods
-    rows = delivery_div.find_all('div', class_='row-delivery')
-    for i in range(0, len(rows), 2):  # Step by 2 to get payment method and its associated delivery options
-        payment_method = rows[i].find('div', class_='col-delivery').text.strip()
-        delivery_options = []
+    driver.get(full_url)
 
-        # Get the delivery options
-        for j in range(i + 1, min(i + 5, len(rows))):  # Limit to 4 delivery options for each payment method
-            img_tag = rows[j].find('img')['src'] if rows[j].find('img') else 'No image'
-            price = rows[j].find('div', class_='delivery-value').text.strip() if rows[j].find('div', class_='delivery-value') else 'No price'
-            delivery_options.append({
-                'method': img_tag,
-                'price': price
-            })
+    driver.execute_script("""
+        var preloader = document.getElementById('pixel-preloader');
+        if (preloader) {
+            preloader.style.display = 'none';
+        }
+    """)
 
-        delivery_data.append({
-            'payment_method': payment_method,
-            'delivery_options': delivery_options
-        })
+    koszty_dostawy_tab = driver.find_element(By.CLASS_NAME, "box_productdeliveries")
+    koszty_dostawy_tab.click()
 
+    driver.implicitly_wait(2)
+
+    delivery_rows = driver.find_elements(By.CLASS_NAME, "row-delivery")
+
+    for row in delivery_rows:
+        delivery_item = {}
+
+        try:
+            image_element = row.find_element(By.TAG_NAME, "img")
+            image_url = image_element.get_attribute("src")
+        except NoSuchElementException:
+            image_url = None
+
+        columns = row.find_elements(By.CLASS_NAME, "col-delivery")
+        if len(columns) == 2:
+            description = columns[0].text.strip()
+            price = columns[1].text.strip()
+
+            if not description and image_url:
+                delivery_item["description"] = image_url.split("/")[-1]
+            else:
+                delivery_item["description"] = description
+
+            delivery_item["price"] = price
+        elif len(columns) == 1:
+            description = columns[0].text.strip()
+
+            delivery_item["description"] = description
+            delivery_item["price"] = None
+
+        delivery_data.append(delivery_item)
     return delivery_data
-
-def scrape_product_enlarge_image(product_url):
-    full_url = site_url + product_url
-    response = requests.get(full_url)
-    
-    if response.status_code == 200:
-        product_soup = BeautifulSoup(response.content, 'html.parser')
-
-         # Zlokalizuj link do powiększonego zdjęcia
-        enlarged_image_tag = product_soup.find('a', class_='js__gallery-anchor-image')
-        enlarged_image_link = site_url + enlarged_image_tag.get('href') if enlarged_image_tag else 'No enlarged image found'
-        
-        return enlarged_image_link
-    else:
-        print(f"Failed to retrieve {full_url}. Status code: {response.status_code}")
-        return None
 
 
 def scrape_product_details(product_url):
     full_url = site_url + product_url
     response = requests.get(full_url)
-    
+
     if response.status_code == 200:
-        product_soup = BeautifulSoup(response.content, 'html.parser')
-        availability_tag = product_soup.find('div', class_='availability')
-        is_available = availability_tag.find('span', class_="second").text.strip() if availability_tag else 'No information'
+        product_soup = BeautifulSoup(response.content, "html.parser")
+        availability_tag = product_soup.find("div", class_="availability")
+        is_available = (
+            availability_tag.find("span", class_="second").text.strip()
+            if availability_tag
+            else None
+        )
 
-        number_of_bought_tag = product_soup.find('div', class_='mx_count')
-        b_tags = number_of_bought_tag.find_all('b') if number_of_bought_tag else None
-        number_of_people = b_tags[0].text.strip() if b_tags else 'No information'
-        number_of_items = b_tags[1].text.strip() if b_tags else 'No information'
+        pixel_coins = product_soup.find("span", class_="points")
+        pixel_coins = pixel_coins.text.strip() if pixel_coins else None
 
-        delivery_div = product_soup.find('div', class_='table-delivery-wrapper')
-        delivery_info = scrape_delivery_info(delivery_div) if delivery_div else 'No delivery information'
+        print(full_url)
+        delivery_info = scrape_delivery_info(full_url)
 
         # Extract product description
-        description_div = product_soup.find('div', class_='resetcss', itemprop='description')
-        description = description_div.get_text(separator=" ", strip=True) if description_div else 'No description'
+        description_div = product_soup.find(
+            "div", class_="resetcss", itemprop="description"
+        )
+        description = (
+            description_div.get_text(separator=" ", strip=True)
+            if description_div
+            else None
+        )
+
+        # Extract enlarge image link
+        enlarged_image_tag = product_soup.find("a", class_="js__gallery-anchor-image")
+        enlarged_image_link = (
+            site_url + enlarged_image_tag.get("href") if enlarged_image_tag else None
+        )
 
         return {
-            'is_available': is_available,
-            'number_of_people': number_of_people,
-            'number_of_items': number_of_items,
-            'delivery_info': delivery_info,
-            'description': description
+            "is_available": is_available,
+            "delivery_info": delivery_info,
+            "description": description,
+            "pixel-coins": pixel_coins,
+            "second image": enlarged_image_link,
         }
     else:
         print(f"Failed to retrieve {full_url}. Status code: {response.status_code}")
         return None
 
-def extract_product_info(product):
-    item_name = product.find('span', class_='productname').text.strip()
-    
-    price_tag = product.find('div', class_='price f-row').find('em')
-    item_price = price_tag.text.strip() if price_tag else ''
-    
-    previous_price_tag = product.find('del', class_='price__inactive')
-    previous_price = previous_price_tag.text.strip() if previous_price_tag else ''
 
-    img_tag = product.find('img')
-    first_image = site_url + img_tag.get('data-src') if img_tag and img_tag.get('data-src') else 'Image not found'
-    
-    details_link = product.find('a').get('href')
-    
-    second_image = scrape_product_enlarge_image(details_link)
+def extract_product_info(product):
+    item_name = product.find("span", class_="productname").text.strip()
+
+    price_tag = product.find("div", class_="price f-row").find("em")
+    item_price = price_tag.text.strip() if price_tag else None
+
+    previous_price_tag = product.find("del", class_="price__inactive")
+    previous_price = previous_price_tag.text.strip() if previous_price_tag else None
+
+    img_tag = product.find("img")
+    first_image = (
+        site_url + img_tag.get("data-src")
+        if img_tag and img_tag.get("data-src")
+        else None
+    )
+
+    details_link = product.find("a").get("href")
 
     detailed_info = scrape_product_details(details_link)
 
     return {
-        'name': item_name,
-        'price': item_price,
-        'previous_price': previous_price,
-        'first_image': first_image,
-        'second_image': second_image,
-        'details': detailed_info
+        "name": item_name,
+        "price": item_price,
+        "previous_price": previous_price,
+        "first_image": first_image,
+        "details": detailed_info,
     }
+
 
 def scrape_category_page(category_url):
     full_url = site_url + category_url
     response = requests.get(full_url)
     scraped_data = []
-    
+
     if response.status_code == 200:
-        category_soup = BeautifulSoup(response.content, 'html.parser')
-        
-        items = category_soup.find_all(class_='product')
+        category_soup = BeautifulSoup(response.content, "html.parser")
+
+        items = category_soup.find_all(class_="product")
 
         for item in items:
             item_info = extract_product_info(item)
@@ -128,10 +197,11 @@ def scrape_category_page(category_url):
         print(f"Failed to retrieve {full_url}. Status code: {response.status_code}")
         return []
 
-def save_to_json(data, filename):
+
+def append_data_to_json(data, filename):
     # Check for existence
     if os.path.exists(filename):
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(filename, "r", encoding="utf-8") as f:
             try:
                 existing_data = json.load(f)
             except json.JSONDecodeError:
@@ -143,45 +213,46 @@ def save_to_json(data, filename):
     existing_data.extend(data)
 
     # Save appended data
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(existing_data, f, ensure_ascii=False, indent=4)
+
 
 # Send a request to the main page
 response = requests.get(site_url)
+setup_page_for_scraping_using_selenium(site_url)
+time.sleep(5)
 
 if response.status_code == 200:
     # Parse the main page content
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    submenu_items = soup.find_all('div', class_='submenu level1')
+    submenu_items = soup.find_all("div", class_="submenu level1")
 
-    all_scraped_data = []  # Lista do przechowywania danych o kategoriach
+    all_scraped_data = []
 
     for submenu in submenu_items:
         # For each submenu, find the links
-        links = submenu.find_all('a')
-        
+        links = submenu.find_all("a")
+
         for link in links:
-            category_name = link.find('span').text.strip()
-            category_link = link.get('href')
+            category_name = link.find("span").text.strip()
+            category_link = link.get("href")
 
             print(f"Scraping category: {category_name}")
             print(f"Link: {category_link}")
-            
-            
+
             # Scrape the individual category page
             category_data = scrape_category_page(category_link)
 
-            # Dodaj kategorię jako listę do głównej listy
-            all_scraped_data.append({
-                'category': category_name,
-                'products': category_data
-            })
+            all_scraped_data.append(
+                {"category": category_name, "products": category_data}
+            )
 
             print(f"Zapisano {len(category_data)} produktów do pliku JSON.")
             # Save the current category data to JSON
-            save_to_json(all_scraped_data[-1:], 'scraped_data.json')  # Zapisz tylko ostatnio dodaną kategorię
-            print('-' * 40)
+            append_data_to_json(all_scraped_data[-1:], "scraped_data.json")
+            print("-" * 40)
+
 
 else:
     print(f"Failed to retrieve the main page. Status code: {response.status_code}")
