8dc07e718271f43dba49dbc62ec476006e442710
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-10-16 16:05:10 +0200

    feat: allowed Merge and Revert commits

--- .github/validate-commits.sh
+++ .github/validate-commits.sh
@@ -6,7 +6,7 @@ echo "Checking commit messages for ${CURRENT_BRANCH} against dev"
 git fetch origin dev:dev
 BASE_BRANCH=$(git merge-base dev ${CURRENT_BRANCH})
 
-if git log --format="%s" $BASE_BRANCH..${CURRENT_BRANCH} | grep -vE '^(init|feat|fix|docs|chore|refactor)( #[0-9]+)?: .{1,100}'
+if git log --format="%s" $BASE_BRANCH..${CURRENT_BRANCH} | grep -vE '^((init|feat|fix|docs|chore|refactor)( #[0-9]+)?: .{1,100}|(Merge|Revert).*)'
 then
     echo "Invalid commit message found."
     exit 1
