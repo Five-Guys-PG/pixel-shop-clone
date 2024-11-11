dde3b1022c9c237b6e93621d92885b6e0286e0f3
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-11-11 18:37:14 +0100

    feat: uploading subcategories using xml template

--- utils/uploader.py
+++ utils/uploader.py
@@ -52,11 +52,24 @@ class Category(PrestaItem):
         except KeyError:
             pass
 
+class Subcategory(Category):
+    def __init__(self, name: str, parent: Category):
+        super().__init__(name)
+        self.name = name
+        self.parent = parent
+
+    def to_xml(self):
+        template = PrestaItem.jinja_env.get_template("subcategory_template.xml")
+        return template.render(name=self.name, parent_id=self.parent.id)
+
+
+def flatten_list(nested_list: list) -> list:
+    return [item for sublist in nested_list for item in sublist]
 
 class Uploader:
     def __init__(self, api_key: str) -> None:
         self._auth = httpx.BasicAuth(username=api_key, password="")
-
+        self._client = httpx.AsyncClient(auth=self._auth)
         self._data = {}
 
     def _add_io_format_json(self, url: str) -> str:
@@ -66,28 +79,38 @@ class Uploader:
         with open(filename, "r") as json_file:
             self._data = json.load(json_file)
 
-    async def create_category(self, category_json: dict) -> Category:
-        async with httpx.AsyncClient(auth=self._auth) as client:
-            category = Category(category_json["category"])
-            category_xml = category.to_xml()
-            response = await client.post(
-                self._add_io_format_json(CATEGORY_API_ENDPOINT),
-                content=category_xml.encode("utf-8"),
-            )
-            category.set_id_from_response(response.json())
-            return category
+    async def create_subcategory(self, subcategory_json: dict, parent: Category) -> Subcategory:
+        subcategory = Subcategory(subcategory_json["subcategory"], parent)
+        subcategory_xml = subcategory.to_xml()
+        response = await self._client.post(
+            self._add_io_format_json(CATEGORY_API_ENDPOINT),
+            content=subcategory_xml.encode("utf-8"),
+        )
+        subcategory.set_id_from_response(response.json())
+        return subcategory
 
-    async def create_categories(self):
-        debug_print("Starting uploading categories")
+    async def create_category(self, category_json: dict) -> list[asyncio.Task]:
+        category = Category(category_json["category"])
+        category_xml = category.to_xml()
+        response = await self._client.post(
+            self._add_io_format_json(CATEGORY_API_ENDPOINT),
+            content=category_xml.encode("utf-8"),
+        )
+        category.set_id_from_response(response.json())
+
+        create_subcategory_tasks = [
+            self.create_subcategory(subcategory, category)
+            for subcategory in category_json["products"]
+        ]
+        return create_subcategory_tasks
+
+    async def run_all(self) -> None:
         create_category_tasks = [
             self.create_category(category) for category in self._data
         ]
-        debug_print(f"Found {len(create_category_tasks)} categories")
-        await asyncio.gather(*create_category_tasks)
-
-    async def run_all(self) -> None:
-        await self.create_categories()
-
+        create_subcategory_tasks = flatten_list(await asyncio.gather(*create_category_tasks))
+        await asyncio.gather(*create_subcategory_tasks)
+        await self._client.aclose()
 
 if __name__ == "__main__":
     uploader = Uploader(api_key=API_KEY)
