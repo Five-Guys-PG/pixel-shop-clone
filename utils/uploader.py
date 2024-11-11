5db4090518d2018d18359160402861c8386fc026
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-11-11 16:59:57 +0100

    feat: uploading category using xml template

--- utils/uploader.py
+++ utils/uploader.py
@@ -1,32 +1,82 @@
-from typing import List, TypedDict
+import asyncio
+import json
+import os
+from pathlib import Path
 
-from prestapyt import PrestaShopWebServiceDict, PrestaShopWebServiceError
+import httpx
+from jinja2 import Environment, FileSystemLoader
 
-product_data = {
-    'product': {
-        'name': {
-            'language': [{'attrs': {'id': '1'}, 'value': 'My Product Name'}]
-        },
-        'price': '19.99',
-        'description': {
-            'language': [{'attrs': {'id': '1'}, 'value': 'This is the product description'}]
-        },
-        'description_short': {
-            'language': [{'attrs': {'id': '1'}, 'value': 'Short description here'}]
-        },
-    }
-}
+API_KEY = os.environ.get("API_KEY")
+
+if API_KEY is None:
+    print("API_KEY env variable is not set")
+    exit(1)
+
+PRESTA_URL = (
+    os.environ["PRESTA_URL"] if "PRESTA_URL" in os.environ else "http://localhost:8080"
+)
+DATA_FILE = Path(__file__).parent.parent / "scrapper" / "scraped_data_category.json"
+XML_TEMPLATES_DIR = Path(__file__).parent / "xml_templates"
+
+CATEGORY_API_ENDPOINT = f"{PRESTA_URL}/api/categories"
+
+
+class PrestaItem:
+    jinja_env = Environment(loader=FileSystemLoader(XML_TEMPLATES_DIR))
+
+    def __init__(self) -> None:
+        self.id: int = -1
+
+
+class Category(PrestaItem):
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+
+    def to_xml(self):
+        template = PrestaItem.jinja_env.get_template("category_template.xml")
+        return template.render(name=self.name)
+
+    def set_id_from_response(self, response: dict):
+        try:
+            self.id = response["category"]["id"]
+        except KeyError:
+            pass
 
 
 class Uploader:
-    def __init__(self, api_url: str, api_key: str):
-        self._prestashop = PrestaShopWebServiceDict(api_url, api_key)
-        blank_schema = self._prestashop.get('products', options={'schema': 'blank'})
-        print(blank_schema)
+    def __init__(self, api_key: str) -> None:
+        self._auth = httpx.BasicAuth(username=api_key, password="")
 
-    def upload_products(self, product_data_source):
-        for product in product_data_source:
-            self._prestashop.add('products', product)
+        self._data = {}
 
-if __name__ == '__main__':
-    uploader = Uploader('http://localhost:8080/api', '6HXFVWL5NH8S9FV5JEAIAILMWR6HSBPV')
+    def _add_io_format_json(self, url: str) -> str:
+        return url + "&io_format=JSON"
+
+    def load_data_from_file(self, filename: str | os.PathLike) -> None:
+        with open(filename, "r") as json_file:
+            self._data = json.load(json_file)
+
+    async def create_category(self, category_json: dict) -> Category:
+        async with httpx.AsyncClient(auth=self._auth) as client:
+            category = Category(category_json["category"])
+            category_xml = category.to_xml()
+            response = await client.post(
+                self._add_io_format_json(CATEGORY_API_ENDPOINT),
+                content=category_xml.encode("utf-8"),
+            )
+            category.set_id_from_response(response.json())
+            return category
+
+    async def create_categories(self):
+        create_category_tasks = [
+            self.create_category(category) for category in self._data["categories"]
+        ]
+        await asyncio.gather(*create_category_tasks)
+
+    async def run_all(self) -> None:
+        await self.create_categories()
+
+
+if __name__ == "__main__":
+    uploader = Uploader(api_key=API_KEY)
