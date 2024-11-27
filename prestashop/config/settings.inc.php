8f0ba46ba884ddfcfef2ae11bebea7b803db6167
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-11-27 09:37:35 +0100

    Revert "feat: added nginx reverse proxy configuration (back office working | front office not)"
    This reverts commit e809ec722ce4d68da4e33e6a45538eee361d2b67.

--- prestashop/config/settings.inc.php
+++ prestashop/config/settings.inc.php
@@ -1,9 +1,2 @@
 <?php
 //@deprecated 1.7
-
-if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-}
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'http') {
-    $_SERVER['HTTPS'] = 'off';
-}
